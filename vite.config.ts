2ac01b4ec55597e13eb2b6d1c0d0c24459694efe
JannePiekkola <janne.piekkola@kuuasema.com>
2026-05-08 00:38:13 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/birds/', // REPLACE with your actual GitHub repository name
+  // Matches your repo name: https://github.com/Alphapolygon/birds
+  base: '/birds/', 
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 });
